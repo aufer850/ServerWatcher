c3d80897e187174c795d939b6218d51df6293611
PC <PC@DESKTOP-EQ2CG60>
2024-05-16 22:08:52 +0300

    Добавьте файлы проекта.

---
+++ ServerWatcher/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace ServerWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Main = new System.Windows.Forms.TabPage();
+            this.Statistics = new System.Windows.Forms.TabPage();
+            this.Search = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.Main.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
+            this.tabControl1.Controls.Add(this.Main);
+            this.tabControl1.Controls.Add(this.Statistics);
+            this.tabControl1.Controls.Add(this.Search);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.tabControl1.ItemSize = new System.Drawing.Size(284, 20);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(884, 461);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.Main.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Main.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Main.Controls.Add(this.label1);
+            this.Main.Location = new System.Drawing.Point(4, 24);
+            this.Main.Name = "Main";
+            this.Main.Padding = new System.Windows.Forms.Padding(3);
+            this.Main.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Main.Size = new System.Drawing.Size(876, 433);
+            this.Main.TabIndex = 0;
+            this.Main.Text = "Головна";
+            this.Main.UseVisualStyleBackColor = true;
+            this.Main.Click += new System.EventHandler(this.Main_Click);
+            // 
+            // Statistics
+            // 
+            this.Statistics.Location = new System.Drawing.Point(4, 24);
+            this.Statistics.Name = "Statistics";
+            this.Statistics.Padding = new System.Windows.Forms.Padding(3);
+            this.Statistics.Size = new System.Drawing.Size(876, 433);
+            this.Statistics.TabIndex = 1;
+            this.Statistics.Text = "Статистика";
+            this.Statistics.UseVisualStyleBackColor = true;
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(4, 24);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(876, 433);
+            this.Search.TabIndex = 2;
+            this.Search.Text = "Пошук";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(413, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.Main.ResumeLayout(false);
+            this.Main.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Main;
+        private System.Windows.Forms.TabPage Statistics;
+        private System.Windows.Forms.TabPage Search;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ServerWatcher/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerWatcher
+{
+    class User
+    {
+        public string Name { get; private set; }
+        public string Password { get; private set; }
+        public Boolean Admin { get; private set; }
+        public User(string N, string P, Boolean A)
+        {
+            Name = N;
+            Password = P;
+            Admin = A;
+        }
+    }
+    
+}
